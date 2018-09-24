2f3d19b3dacf385e13910a300ab877bd5437e743
hyuraku <yu@hill-wood.com>
2018-09-24 15:27:55 +0900

    create author_type

---
+++ app/graphql/types/author_type.rb
@@ -0,0 +1,16 @@
+module Types
+  class AuthorType < Types::BaseObject
+    description "An author"
+
+    field :id, ID,null: false
+    field :first_name, String,null: true,camelize: false
+    field :last_name, String,null: true,camelize: false
+    field :birth_year, Int,null: false
+    field :is_alive, Boolean,null: true,camelize: false
+    field :full_name, String,null: true,camelize: false
+
+    def full_name
+      ([object.first_name,object.last_name].compact).join
+    end
+  end
+end
