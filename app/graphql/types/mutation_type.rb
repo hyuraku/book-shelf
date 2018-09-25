d5744d1298c1be86487df65de8bbfe421bf23552
hyuraku <yu@hill-wood.com>
2018-09-25 23:24:54 +0900

    modify create method

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,10 +2,10 @@ module Types
   class MutationType < Types::BaseObject
 
     field :create_author, AuthorType,null: true,description: "Create an author" do
-      field :first_name, String,required: true,camelize: false
-      field :last_name, String,required: true,camelize: false
-      field :birth_year, Int,required: false
-      field :is_alive, Boolean,required: true,camelize: false
+      argument :first_name, String,required: false,camelize: false
+      argument :last_name, String,required: false,camelize: false
+      argument :birth_year, Int,required: false
+      argument :is_alive, Boolean,required: false,camelize: false
     end
 
     def create_author(first_name: ,last_name:,birth_year:,is_alive:)
